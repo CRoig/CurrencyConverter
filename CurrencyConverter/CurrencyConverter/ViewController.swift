23d9f1450e7d24fcadbfcce9b98a3c4b3475b362
Carles Roig <carlesroiggarcia@gmail.com>
2018-09-09 12:12:03 +0200

    Implement Currency Update service

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    private var baseCurrency = "EUR"
+    private var rates = [String: Any]()
+    
+    @IBOutlet private weak var tableView: UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        CurrencyService().getCurrencyRates(base: baseCurrency, completion: { currencyUpdate in
+            if (currencyUpdate == nil) {
+                //TODO: Add error reporting
+            }else{
+                self.baseCurrency = currencyUpdate!.baseCurrency
+                self.rates = currencyUpdate!.rates
+                self.tableView?.reloadData()
+            }
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: UITableViewDelegate
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Array(rates.keys).count + 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var title = ""
+        if indexPath.row == 0 {
+            title = baseCurrency
+        }else{
+            title = Array(rates.keys)[indexPath.row - 1]
+        }
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = title
+        
+        return cell
+    }
 }
-
