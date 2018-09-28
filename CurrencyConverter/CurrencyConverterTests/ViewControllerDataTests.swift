4de545bc99cad20926455a287c836e955e60d4b1
Carles Roig <carlesroiggarcia@gmail.com>
2018-09-28 17:05:45 +0200

    Add new rate local conversion test

--- CurrencyConverter/CurrencyConverterTests/ViewControllerDataTests.swift
+++ CurrencyConverter/CurrencyConverterTests/ViewControllerDataTests.swift
@@ -53,16 +53,23 @@ class ViewControllerTests: XCTestCase {
     func testSwitchCurrency() {
         let firstCurrency = viewController.rates[0]
         let firstInitialDisplay = firstCurrency.displayValue()
+        let firstInitialRate = firstCurrency.rate
         let secondCurrency = viewController.rates[1]
         let secondInitialDisplay = secondCurrency.displayValue()
+        let secondInitialRate = secondCurrency.rate
         let thirdCurrency = viewController.rates[2]
         let thirdInitialDisplay = thirdCurrency.displayValue()
+        let thirdInitialRate = thirdCurrency.rate
         
         viewController.updateBaseCurrency(at: IndexPath.init(row: 2, section: 0))
         
         XCTAssertEqual(viewController.rates[0].name, thirdCurrency.name)
         XCTAssertEqual(viewController.rates[1].name, firstCurrency.name)
         XCTAssertEqual(viewController.rates[2].name, secondCurrency.name)
+        
+        XCTAssertEqual(viewController.rates[0].rate, 1.0)
+        XCTAssertEqual(viewController.rates[1].rate, firstInitialRate / thirdInitialRate)
+        XCTAssertEqual(viewController.rates[2].rate, secondInitialRate / thirdInitialRate)
         
         XCTAssertEqual(viewController.rates[0].displayValue(), thirdInitialDisplay)
         XCTAssertEqual(viewController.rates[1].displayValue(), firstInitialDisplay)
